142fbf153e3c09bca8e9ad48ced9ba9cfdd3f239
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-10-15 14:07:34 -0400

    Separated css into separate files

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -1,3 +1,5 @@
+import "../styles/ActivityCard.css";
+
 const ActivityCard = () => {
     return (
         <td className="ActivityCard">
--- src/routes/MapPage.jsx
+++ src/routes/MapPage.jsx
@@ -1,4 +1,6 @@
 import { GoogleMap, useLoadScript} from "@react-google-maps/api";
+import "../styles/MapPage.css";
+
 const Map = () => {
 
     const { isLoaded } = useLoadScript ({googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,})
