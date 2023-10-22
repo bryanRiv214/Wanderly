68c55ff2a4d4de31a241e04813392dea7d5eec4a
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-10-22 00:37:33 -0400

    Styled activity card with placeholder data

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -1,10 +1,24 @@
 import "../styles/ActivityCard.css";
 
-const ActivityCard = () => {
+const ActivityCard = (title, location, text, imageURL, tags, weather, type) => {
     return (
         <td className="ActivityCard">
             <div className="activity-content">
-                Activity Content
+                <small className="activity-card-timestamp">24 October 2023</small>
+                <h2 className="activity-card-title">Activity Title</h2>
+
+                <img className="activity-card-img" alt="" src="https://media.cntraveler.com/photos/611fc78d9282cc5de31e9d87/16:9/w_2240,c_limit/Central%20Park,%20Manhattan,%20New%20York_GettyImages-528180834.jpg"></img>
+
+                <p className="activity-card-location">📍 Location</p>
+
+                <div className="activity-card-tags-container">
+                    <h3>Tags: </h3>
+                    <ul className="activity-card-tags-list">
+                        <li>#outdoor</li>
+                        <li>#walking</li>
+                        <li>#nature</li>
+                    </ul>
+                </div>
             </div>
         </td>
     )
