2408ce599053b80d90cfb4e395bea5b0a86daa5e
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-10-22 01:07:44 -0400

    Changed structure of activities table

--- src/components/ActivitiesTable.jsx
+++ src/components/ActivitiesTable.jsx
@@ -1,39 +1,46 @@
 import ActivityCard from "./ActivityCard";
 import "../styles/ActivitiesTable.css";
+import { useState, useEffect } from "react";
 
 const ActivitiesTable = () => {
+    useEffect(()=>{
+        getData();
+    }, [])
+
+    const [listOfPosts, setListOfPosts] = useState([]);
+
+    const getData=()=>{
+        fetch('sampledata.json',{
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        }
+        )
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(data) {
+            setListOfPosts(data.posts);
+            console.log(data.posts)
+        });
+    }
+
     return (
-        <table className="ActivitiesTable">
-            <tbody>
-                {/* First row */}
-                <tr>
-                    <ActivityCard />
-
-                    <ActivityCard />
-                </tr>
-
-                {/* Second row */}
-                <tr>
-                    <ActivityCard />
-
-                    <ActivityCard />
-                </tr>
-
-                {/* Third row */}
-                <tr>
-                    <ActivityCard />
-
-                    <ActivityCard />
-                </tr>
-
-                {/* Fourth row */}
-                <tr>
-                    <ActivityCard />
-
-                    <ActivityCard />
-                </tr>
-            </tbody>
-        </table>
+        <div className="ActivitiesContainer">
+            {listOfPosts? 
+                listOfPosts.map(post => 
+                <ActivityCard 
+                key={post.post_id} 
+                title={post.title}
+                location={post.location}
+                imageURL={post.image}
+                weather={post.weather} 
+                tags={post.tags}
+                />)
+            : ""
+            }
+        </div>
     )
 }
 
