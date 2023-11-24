d438d885bc56ff5960941413f2f1fa0d0aa8b70a
akivarichman <121504299+akivarichman@users.noreply.github.com>
2023-11-24 11:15:12 -0500

    uspdate duration section of form

--- src/components/CreatePostForm.jsx
+++ src/components/CreatePostForm.jsx
@@ -4,12 +4,13 @@ import '../styles/CreatePostForm.css';
 function CreatePostForm() {
 
     const weatherOptions = ["Sunny", "Cloudy", "Rainy", "Snowy", "Windy", "Cold", "Any"];
+    const DurationOptions = ["0 - 30 min", "30 - 60 min", "1 - 1.5 hrs", "1.5 - 2 hrs", "2 - 4 hrs", "4 - 8 hrs", "All Day"]
     const activityTypeOptions = ["Indoor", "Outdoor", "Family-Friendly", "Kids", "Adults", "Nightlife", "Relaxing", "Creative", "Shopping", "Sports", "Arts and Crafts", "Guided Tours", "Museums and Education", "Parks and Recreation", "Wellness and Health", "Nature and Hiking", "Food and Drink", "Historical Landmarks", "Festivals and Events", "Other"];
     const locationOptions = ["New York, NY", "Chicago, IL", "Boston, MA", "Philadephia, PA", "Los Angeles, CA"];
 
-    const[activity, setActivity] = useState('');
-    const handleActivityChange = (e) => {
-        setActivity(e.target.value)
+    const[title, setTitle] = useState('');
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value)
     }
     const[description, setDescription] = useState('');
     const handleDescriptionChange = (e) => {
@@ -19,27 +20,19 @@ function CreatePostForm() {
     const handleLocationChange = (e) => {
         setLocation(e.target.value)
     }
-    const[weather, setWeather] = useState('');
-    const handleWeatherChange = (e) => {
-        setWeather(e.target.value)
-    }
-    const[minDuration, setMinDuration] = useState('');
-    const handleMinDurationChange = (e) => {
-        if(/^[0-9]*$/.test(e.target.value)) {
-            setMinDuration(e.target.value)
-        }
-    }
-    const[maxDuration, setMaxDuration] = useState('');
-    const handleMaxDurationChange = (e) => {
-        if(/^[0-9]*$/.test(e.target.value)) {
-            setMaxDuration(e.target.value)
-        }
-    }
     const[price, setPrice] = useState('');
     const handlePriceChange = (e) => {
         if(/^[0-9]*$/.test(e.target.value)) {
             setPrice(e.target.value)
         }
+    }
+    const [duration, setDuration] = useState('');
+    const handleDurationChange = (e) => {
+        setDuration(e.target.value)
+    }
+    const[weather, setWeather] = useState('');
+    const handleWeatherChange = (e) => {
+        setWeather(e.target.value)
     }
     const[type, setType] = useState('');
     const handleTypeChange = (e) => {
@@ -48,7 +41,7 @@ function CreatePostForm() {
 
     const handleSubmit = (e) => {
         e.preventDefault(); // stops the page from refreshing upon submit
-        const newPost = {activity, description, location, minDuration, maxDuration, price, weather, type, date: new Date().toLocaleString(), likes: 0};
+        const newPost = {title, description, location, duration, price, weather, type, date: new Date().toLocaleString(), likes: 0};
         // error checking
         if(!isFormValid(newPost)) {
             console.log("no good")
@@ -56,20 +49,16 @@ function CreatePostForm() {
         }
         console.log(newPost)
         // reset state variables
-        setActivity('')
+        setTitle('')
         setDescription('');
         setLocation('');
-        setMinDuration('');
-        setMaxDuration('');
+        setDuration('');
         setPrice('');
         setWeather('');
         setType('');
     }
 
     function isFormValid(newPost) {
-        if(newPost.minDuration >= newPost.maxDuration) {
-            return false;
-        }
         return true;
     }
 
@@ -77,9 +66,9 @@ function CreatePostForm() {
         <div>
             <div className='tab'>Create Post</div>
             <form className='newPostForm' name='newPost' onSubmit={handleSubmit}>
-                <div className='ActivitySection'>
+                <div className='TitleSection'>
                     <label>Activity:</label>
-                    <input className='input' type='text' name='activity' placeholder='Title' value={activity} onChange={handleActivityChange} maxLength='50' autoComplete='off' required></input>
+                    <input className='input' type='text' name='activity' placeholder='Title' value={title} onChange={handleTitleChange} maxLength='50' autoComplete='off' required></input>
                 </div>
                 <br/>
                 <div className='DescriptionSection'>
@@ -97,18 +86,25 @@ function CreatePostForm() {
                     </select>
                 </div>
                 <br/>
-                <div className='DurationSection'>
-                    <label>Duration (hrs):</label>
-                    <input className='input' type='text' name='minDuration' placeholder='##' value={minDuration} onChange={handleMinDurationChange} maxLength='2' dir='rtl' autoComplete='off' required></input>
-                    <p>-</p>
-                    <input className='input' type='text' name='maxDuration' placeholder='##' value={maxDuration} onChange={handleMaxDurationChange} maxLength='2' dir='rtl' autoComplete='off' required></input>
-                </div>
-                <br/>
                 <div className='PriceSection'>
                     <label>Estimated Price: $</label>
                     <input className='input' type='text' name='price' placeholder='###' value={price} onChange={handlePriceChange} maxLength='4' dir='rtl' autoComplete='off' required></input>
                 </div>
-                <br/> 
+                <br/>
+                <div className='DurationSection'>
+                    <label>Duration (hrs):</label>
+                    <div className='options'>
+                        {DurationOptions.map((item, index) => 
+                            <div key={index}>
+                                <label>
+                                    <input className='input' type='radio' name='duration' value={item} onChange={handleDurationChange} checked={duration===item} required></input>
+                                    {item}
+                                </label>
+                            </div>   
+                        )}
+                    </div>
+                </div>
+                <br/>
                 <div className='WeatherSection'>
                     <label>Weather:</label>
                     <div className='options'>
@@ -152,9 +148,31 @@ export default CreatePostForm;
 // dropdown options can be done better
 /* 
 User ID should be automatic
-Empty comments
 */
 // do I need a checked attribute for the radio buttons
 
-// consider an "other" option
-// add a free button
+
+
+
+/*
+const[minDuration, setMinDuration] = useState('');
+    const handleMinDurationChange = (e) => {
+        if(/^[0-9]*$/.test(e.target.value)) {
+            setMinDuration(e.target.value)
+        }
+    }
+    const[maxDuration, setMaxDuration] = useState('');
+    const handleMaxDurationChange = (e) => {
+        if(/^[0-9]*$/.test(e.target.value)) {
+            setMaxDuration(e.target.value)
+        }
+    }
+
+
+    <div className='DurationSection'>
+        <label>Duration (hrs):</label>
+        <input className='input' type='text' name='minDuration' placeholder='##' value={minDuration} onChange={handleMinDurationChange} maxLength='2' dir='rtl' autoComplete='off' required></input>
+        <p>-</p>
+        <input className='input' type='text' name='maxDuration' placeholder='##' value={maxDuration} onChange={handleMaxDurationChange} maxLength='2' dir='rtl' autoComplete='off' required></input>
+    </div>
+*/
