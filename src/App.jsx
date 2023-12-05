a23dda163b12e59c105db6abfcb1dbc6ddc4746e
akivarichman <121504299+akivarichman@users.noreply.github.com>
2023-12-05 14:40:43 -0500

    finally ready to adapt to our needs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react';
 import Header from './components/Header';
-import Homepage from './routes/Homepage';
+import HomePage from './routes/HomePage'
 import PageNotFound from './routes/PageNotFound';
 import ProfilePage from './routes/ProfilePage';
 import Footer from './components/Footer';
-import Map from './routes/MapPage'
+import Map from './routes/MapPage';
 import { Route, Routes } from 'react-router-dom';
 import Login from './routes/LoginPage';
-import SignUp from './routes/SignUpPage'
+import SignUp from './routes/SignUpPage';
 
 const App = () => {
 
@@ -29,7 +29,11 @@ const App = () => {
         <div>
             <Header />
             <Routes>
-                <Route index element={<Homepage userName="Bryan" location="New York City"/>} />
+                {token ?
+                <Route index element={<HomePage userName="Bryan" location="New York City"/>} />
+                :
+                ''
+                }
                 <Route path="*" element={<PageNotFound />} />
                 <Route path="profile" element={<ProfilePage/>} />
                 <Route path = "map" element = {<Map/>}/>
