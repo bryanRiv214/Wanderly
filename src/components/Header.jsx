239cd51850c24d0673657446d9a39a575f49468a
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-12-05 15:54:05 -0500

    Header responsive for x-small devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,15 +11,13 @@ const Header = () => {
             <h2 className='wanderly-title'>Wanderly</h2>
         </div>
 
-        <div className="navbar-links-container">
-            <ul className="navbar-links-list">
-                {/* Paste links to other pages when we are done creating them */}
-                {/* They have to match the paths. See index.js */}
-                <Link to='/' className="navbar-link">Home</Link>
-                <Link to='/map' className="navbar-link">Map</Link>
-                <Link to='/profile' className="navbar-link">Profile</Link>
-            </ul>
-        </div>
+        <ul className="navbar-links-container">
+            {/* Paste links to other pages when we are done creating them */}
+            {/* They have to match the paths. See index.js */}
+            <Link to='/' className="navbar-link">Home</Link>
+            <Link to='/map' className="navbar-link">Map</Link>
+            <Link to='/profile' className="navbar-link">Profile</Link>
+        </ul>
     </nav>
     )
 }
