82566afdc1cc6207c4b566cbff4ff8710b4a56d0
akivarichman <121504299+akivarichman@users.noreply.github.com>
2023-12-05 13:24:20 -0500

    final merge of logout

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
-function Homepage() {
+// Starting page
 
+const Homepage = ({userName, location}) => {
+  return (
+    <div className="Homepage">
+      <h1 className="good-morning-msg">Good Morning, {userName}</h1>
+
+      <SearchBar />
+
+      <h2>What can you do today?</h2>
+      
     const navigate = useNavigate()
 
     function handleLogout() {
