db8b15b18c39b4a612acb582b9064d576dab597d
akivarichman <121504299+akivarichman@users.noreply.github.com>
2023-12-05 14:09:55 -0500

    .

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import ActivitiesTable from "../components/ActivitiesTable";
+import SearchBar from "../components/SearchBar";
+import "../styles/Homepage.css";
 
 // Starting page
 
 const Homepage = ({userName, location}) => {
+
+  let navigate = useNavigate();
+
+  function handleLogout() {
+    sessionStorage.removeItem('token')
+    navigate('/login')
+  }
+
   return (
     <div className="Homepage">
       <h1 className="good-morning-msg">Good Morning, {userName}</h1>
+
+      <button onClick={handleLogout}>Logout</button>
 
       <SearchBar />
 
       <h2>What can you do today?</h2>
       
-    const navigate = useNavigate()
+      <ActivitiesTable />
 
-    function handleLogout() {
-        sessionStorage.removeItem('token');
-        navigate('/login')
-    }
+      {/* Replace with a better image. This image was taken from FontAwesome.com */}
+      <img className='arrow-down-img' src='arrow-down-solid.svg' alt=''></img>
 
     return (
         <div>
             Homepage
             <button onClick={handleLogout}>Logout</button>
-        </div>
-    );
+    </div>
+  );
 }
 
-export default Homepage;
+export default Homepage; 
