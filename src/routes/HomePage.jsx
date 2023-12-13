29bed5842d41879ed2687a5e61bd65984c5effd2
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-12-12 23:26:24 -0500

    Merged homepage

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import Logout from '../components/Logout';
 import ActivitiesTable from "../components/ActivitiesTable";
 import SearchBar from "../components/SearchBar";
 import "../styles/Homepage.css";
@@ -6,15 +9,32 @@ import "../styles/Homepage.css";
 // Starting page
 
 const HomePage = ({userName, location}) => {
+  const [city, setCity] = useState();
+  let navigate = useNavigate();
+
+  function handleLogout() {
+    sessionStorage.removeItem('token')
+    navigate('/login')
+  }
+
+  // function to record city selected
+  const handleCitySelect = (city) => {
+      setCity(city);
+      console.log("Homepage:" + city);
+  }
+
   return (
     <div className="Homepage">
       <h1 className="good-morning-msg">Good Morning, {userName}</h1>
 
-      <SearchBar />
+      <Logout />
+
+      <SearchBar onCitySelect={handleCitySelect}/>
 
       <h2>What can you do today?</h2>
       
-      <ActivitiesTable />
+      {/* Searchbar has a callback function to record city name */}
+      <ActivitiesTable selectedCity={city}/>
 
       {/* Replace with a better image. This image was taken from FontAwesome.com */}
       <img className='arrow-down-img' src='arrow-down-solid.svg' alt=''></img>
