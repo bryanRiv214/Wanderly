70092f6d2c86ef745569908c65500c626edbf8c1
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-12-12 20:08:06 -0500

    Semi-responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import "../styles/Footer.css";
 
 const Footer = () => {
     return (
-        <footer className="Footer">
-            <div className="footer-row-container">
-                <h3 className="slogan">
-                    Explore the world <br></br>
+        <footer className="Footer container-fluid">
+            <div className="footer-row-container row">
+                <h3 className="slogan col-sm-12 col-md-6">
+                    Explore the world
                     with <span className="wanderly-beige">Wanderly</span>
                 </h3>
 
-                <div className="social-media-icons-container">
+                <div className="social-media-icons-container col-sm-12 col-md-6">
                     <img src='linkedin.svg' className="social-media-icon" alt='linked-in logo'></img>
                     <img src='twitter.svg' className="social-media-icon" alt='twitter logo'></img>
                     <img src='square-instagram.svg' className="social-media-icon" alt='instagram logo'></img>
@@ -17,12 +17,12 @@ const Footer = () => {
                 </div>
             </div>
 
-            <div className="footer-row-container">
-                <p>wanderly@domain.com</p>
+            <div className="website-info-container footer-row-container row col-sm-12 col-md-6">
+                <p className="col-sm-12 col-md-6">wanderly@domain.com</p>
 
-                <div className="terms-and-privacy-container">
-                    <p>Terms & Conditions</p>
-                    <p>Privacy Statement</p>
+                <div className="terms-and-privacy-container col-sm-12 col-md-6 row">
+                    <p className="col-sm-6">Terms & Conditions</p>
+                    <p className="col-sm-6"> Privacy Statement</p>
                 </div>
             </div>
         </footer>
