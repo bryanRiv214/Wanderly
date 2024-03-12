3d63aedaec93e38676fc0af5bb20ff597c3a4eba
akivarichman <121504299+akivarichman@users.noreply.github.com>
2024-03-12 13:52:34 -0400

    saving the selected sorting choice in a sort state

--- src/components/SortDropdown.jsx
+++ src/components/SortDropdown.jsx
@@ -6,15 +6,18 @@ import "../styles/SortDropdown.css";
 
 const SortDropdown = () => {
 
-    const[sort, setSort] = useState('Popular')
-    const title = `Sort By: ${sort}`;
+    const[sort, setSort] = useState('Popular');
+
+    const handleSelect = (eventKey) => {
+        setSort(eventKey);
+    }
 
     return (
         <div className="sort-dropdown">
-            <DropdownButton id='dropdown-button' title={title}>
-                <Dropdown.Item>Price</Dropdown.Item>
-                <Dropdown.Item>Popular</Dropdown.Item>
-                <Dropdown.Item>Third option</Dropdown.Item>
+            <DropdownButton id='dropdown-button' title={`Sort By: ${sort}`} onSelect={handleSelect}>
+                <Dropdown.Item eventKey="Price">Price</Dropdown.Item>
+                <Dropdown.Item eventKey="Popular">Popular</Dropdown.Item>
+                <Dropdown.Item eventKey="Newest">Newest</Dropdown.Item>
             </DropdownButton>
         </div>
     )
